6c1a6d70d2af033a9b1aec411a2e1e12d7b9235e
ashachakre0906 <chourpagar.asha@gmail.com>
2022-09-03 13:28:31 -0700

    added CSS loaders and abbel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,21 +9,41 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 module.exports = () => {
   return {
     mode: 'development',
+    //Entry point for files
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+    //Output for all our bundles
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      //Webpack plugin that generates our html file and inject our bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Just Another Text Editor'
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+        },
+        {   
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          //We use babel loadder in order to use ES6
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugin: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          }
+        }
       ],
     },
   };
