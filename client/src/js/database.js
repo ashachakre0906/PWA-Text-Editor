a7650bf9871193b66da83e2baf595dc401c1e86c
ashachakre0906 <chourpagar.asha@gmail.com>
2022-09-08 16:47:34 -0700

    added and configure workbox plugins for a service worker and manifest file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,40 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+//We are creating a new database named jate which will be using version 1 of the database
   openDB('jate', 1, {
+// Add our databse schema if it is has not already been initialized
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      // create a new object store for the data and give it an key name of 'id' which needs to be incremental 
+      // automatically
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // console.error('putDb not implemented')
+    const JateDB = await openDB("jate", 1);
+    const tx = JateDB.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
+    const request = store.put({jate:content})
+    const result = await request
+    console.log("Await my request and save data to DB", result)
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const JateDB = await openDB("jate", 1);
+  const tx = JateDB.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll()
+  const result = await request
+  console.log("RESULTS: ", result)
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,73 +1,147 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const WebpackPwaManifest = require('webpack-pwa-manifest');
+// const path = require('path');
+// const { InjectManifest } = require('workbox-webpack-plugin');
+
+// // TODO: Add and configure workbox plugins for a service worker and manifest file.
+// // TODO: Add CSS loaders and babel to webpack.
+
+// module.exports = () => {
+//   return {
+//     mode: 'development',
+//     //Entry point for files
+//     entry: {
+//       main: './src/js/index.js',
+//       install: './src/js/install.js'
+//     },
+//     //Output for all our bundles
+//     output: {
+//       filename: '[name].bundle.js',
+//       path: path.resolve(__dirname, 'dist'),
+//     },
+//     plugins: [
+//       //Webpack plugin that generates our html file and inject our bundles
+//       new HtmlWebpackPlugin({
+//         template: './index.html',
+//         title: 'Just Another Text Editor'
+//       }),
+//       //Inject our custom service worker
+//       new InjectManifest({
+//         swSrc: './src-sw.js',
+//         swDest: 'src-sw.js',
+//       }),
+//       new WebpackPwaManifest({
+//         fingerprints: false,
+//         inject: true,
+//         name: 'Text Editor',
+//         short_name: 'TE',
+//         description: 'Just another text editor!',
+//         background_color: '#225ca3',
+//         theme_color: '#225ca3',
+//         start_url: '/',
+//         publicPath: '/',
+//         icons: [
+//           {
+//             src: path.resolve('src/images/logo.png'),
+//             sizes: [96, 128, 192, 256, 384, 512],
+//             destination: path.join('assets', 'icons'),
+//           },
+//         ],
+//       }),
+//     ],
+//     module: {
+//       //CSS loaders
+//       rules: [
+//         {
+//         test: /\.css$/i,
+//         use: ['style-loader', 'css-loader'],
+//         },
+//         {
+//           test: /\.m?js$/,
+//           exclude: /node_modules/,
+//           //We use babel loadder in order to use ES6
+//           use: {
+//             loader: 'babel-loader',
+//             options: {
+//               presets: ['@babel/preset-env'],
+//               plugin: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+//             },
+//           }
+//         }
+//       ],
+//     },
+//   };
+// };
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
-    //Entry point for files
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
-    //Output for all our bundles
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       //Webpack plugin that generates our html file and inject our bundles
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Just Another Text Editor'
+        template: "./index.html",
+        title: "JATE",
       }),
       //Inject our custom service worker
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Text Editor',
-        short_name: 'TE',
-        description: 'Just another text editor!',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
+        name: "Just another text editor",
+        short_name: "JATE",
+        description: "Just another text editor!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
     ],
     module: {
-      //CSS loaders
       rules: [
         {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
         },
-        {   
+        {
           test: /\.m?js$/,
           exclude: /node_modules/,
           //We use babel loadder in order to use ES6
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugin: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              presets: ["@babel/preset-env"],
+              plugin: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
-          }
-        }
+          },
+        },
       ],
     },
   };
