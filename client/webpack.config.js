c421cddfeaea1cdbf07a6e2a4bb30f5b5b0d8778
ashachakre0906 <chourpagar.asha@gmail.com>
2022-09-11 19:42:09 -0700

    updated package.json and added node and npm version

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,77 +1,3 @@
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const WebpackPwaManifest = require('webpack-pwa-manifest');
-// const path = require('path');
-// const { InjectManifest } = require('workbox-webpack-plugin');
-
-// // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// // TODO: Add CSS loaders and babel to webpack.
-
-// module.exports = () => {
-//   return {
-//     mode: 'development',
-//     //Entry point for files
-//     entry: {
-//       main: './src/js/index.js',
-//       install: './src/js/install.js'
-//     },
-//     //Output for all our bundles
-//     output: {
-//       filename: '[name].bundle.js',
-//       path: path.resolve(__dirname, 'dist'),
-//     },
-//     plugins: [
-//       //Webpack plugin that generates our html file and inject our bundles
-//       new HtmlWebpackPlugin({
-//         template: './index.html',
-//         title: 'Just Another Text Editor'
-//       }),
-//       //Inject our custom service worker
-//       new InjectManifest({
-//         swSrc: './src-sw.js',
-//         swDest: 'src-sw.js',
-//       }),
-//       new WebpackPwaManifest({
-//         fingerprints: false,
-//         inject: true,
-//         name: 'Text Editor',
-//         short_name: 'TE',
-//         description: 'Just another text editor!',
-//         background_color: '#225ca3',
-//         theme_color: '#225ca3',
-//         start_url: '/',
-//         publicPath: '/',
-//         icons: [
-//           {
-//             src: path.resolve('src/images/logo.png'),
-//             sizes: [96, 128, 192, 256, 384, 512],
-//             destination: path.join('assets', 'icons'),
-//           },
-//         ],
-//       }),
-//     ],
-//     module: {
-//       //CSS loaders
-//       rules: [
-//         {
-//         test: /\.css$/i,
-//         use: ['style-loader', 'css-loader'],
-//         },
-//         {
-//           test: /\.m?js$/,
-//           exclude: /node_modules/,
-//           //We use babel loadder in order to use ES6
-//           use: {
-//             loader: 'babel-loader',
-//             options: {
-//               presets: ['@babel/preset-env'],
-//               plugin: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-//             },
-//           }
-//         }
-//       ],
-//     },
-//   };
-// };
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
@@ -83,7 +9,7 @@ const { InjectManifest } = require("workbox-webpack-plugin");
 module.exports = () => {
   return {
     mode: "development",
-    watch: (process.argv.indexOf('--watch') > -1),
+    // watch: (process.argv.indexOf('--watch') > -1),
     entry: {
       main: "./src/js/index.js",
       install: "./src/js/install.js",
