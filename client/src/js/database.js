a9723ce2e224d8ab27c26dbe6322e67a6ce35212
ashachakre0906 <chourpagar.asha@gmail.com>
2022-09-12 22:51:16 -0700

    updated code and added comments in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,12 +18,16 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log(content);
-  // console.error('putDb not implemented');
+  console.log('Post to the database');
+  // Create a connection to the databse and version we want to use
     const JateDB = await openDB("jate", 1);
+  // Create a new transaction and specify the database and data privileges
     const tx = JateDB.transaction("jate", "readwrite");
+  // Open up the desired object store
     const store = tx.objectStore("jate");
+  //The put() method of the IDBObjectStore interface updates a given record in a database, or inserts a new record if the given item does not already exist.
     const request = store.put({jate:content});
+  //Get confirmation of the request
     const result = await request;
     console.log("Await my request and save data to DB", result);
 
@@ -31,12 +35,18 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+// Create a connection to the database and version we want to use
   const JateDB = await openDB("jate", 1);
+// Creating a new transaction and specify the database and data privileges
   const tx = JateDB.transaction("jate", "readonly");
+// Open up desired object store
   const store = tx.objectStore("jate");
+// Use .getAll()method to get the data from the database
   const request = store.getAll();
+// Get confirmation of the request
   const result = await request;
-  console.log("RESULTS: ", result);
+  console.log("Get All the contents", result);
 };
+// Start the database
 
 initdb();
