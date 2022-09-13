41709b7b85af6ab240e7be061ebc677b54cce8d2
ashachakre0906 <chourpagar.asha@gmail.com>
2022-09-12 23:39:02 -0700

    updated install.js code because my install button is broken on app

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,29 +3,30 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    //Store the triggered events
+    // Store the event data
     window.deferredPrompt = event;
-    //Removed the hidden class from the button
-    butInstall.classList.toggle("hidden", false)
+    // remove hidden class from the install button
+    butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    console.log('button is clicked');
+    console.log('Install Button clicked');
     const promptEvent = window.deferredPrompt;
-    console.log(promptEvent);
-    if(!promptEvent){
+    // If there is no window event, return
+    if (!promptEvent) {
         return;
     }
-    // Show prompt
+
     promptEvent.prompt();
-//Reset the deffered prompt variable, it can only be used once.
+
+    // reset the window deferred prompt back to null
     window.deferredPrompt = null;
-    butInstall.classList.toggle("hidden", true)
+
+    butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    //Clear prompt
     window.deferredPrompt = null;
 });
